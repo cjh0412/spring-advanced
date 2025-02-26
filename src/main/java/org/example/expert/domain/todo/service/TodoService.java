ddbfbad6d17542d473a4eb04f9ec337ee797f3d4
jyedev04 <jyedev04@gmail.com>
2025-02-26 17:48:06 +0900

    feat(static) : 정적 메서드 추가

--- src/main/java/org/example/expert/domain/todo/service/TodoService.java
+++ src/main/java/org/example/expert/domain/todo/service/TodoService.java
@@ -17,6 +17,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.example.expert.domain.common.exception.CommonErrorCode.TODO_NOT_FOUND;
+
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
